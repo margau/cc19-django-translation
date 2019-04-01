7f07fb0ff74f8a7ee578afe637a396333deb21d8
Marvin Gaube <dev@marvingaube.de>
2019-04-01 14:58:40 +0200

    Basic POST Handling

--- backend/views.py
+++ backend/views.py
@@ -4,8 +4,13 @@ from django.template import loader
 # Create your views here.
 def index(request):
     template = loader.get_template('index.html')
+    form = request.POST
     context = {
         'translated': False,
         'translation': 'Test-Übersetzung',
     }
+    if form.get('input'):
+        # ToDo: Translate-
+        context['translation'] = form.get('input')
+        context['translated'] = True
     return HttpResponse(template.render(context, request))
