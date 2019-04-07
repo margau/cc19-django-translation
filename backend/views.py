7e2aa538338a175560708030f1262b2002dc2d81
Marvin Gaube <dev@marvingaube.de>
2019-04-07 17:33:10 +0200

    Add some comments

--- backend/views.py
+++ backend/views.py
@@ -2,18 +2,22 @@ from django.http import HttpResponse
 from django.template import loader
 from google.cloud import translate
 
+# Initialize Google Translate obj.
 client = translate.Client(target_language='de')
 
-# Create your views here.
+# Everything is realized in the index view
 def index(request):
     template = loader.get_template('index.html')
+    # Get POST-Data if available
     form = request.POST
+    # Initialize context
     context = {
         'translated': False,
         'translation': '',
         'source_lang': 'de',
         'orig': '',
     }
+    # If form is transmitted (POST), translate and fill out context
     if form.get('input'):
         t = client.translate(form.get('input'))
         context['translation'] = t['translatedText']
